d961566f826630e1767e6fc22f32552b27cdbfec
amadjarova <117368351+amadjarova@users.noreply.github.com>
2024-07-28 12:42:08 +0300

    Update OOPproject.cpp

--- OOPproject.cpp
+++ OOPproject.cpp
@@ -1,38 +1,26 @@
-
 #include <iostream>
 #include <vector>
 #include "SocialNetwork.h"
-int main()
-{
-    SocialNetwork sc;
-    while (true)
-    {
+
+int main() {
+    SocialNetwork sn;
+    
+    while (true) {
         char buff[1024];
         std::cin.getline(buff, 1024);
-        if (strcmp(buff, ">signup") == 0)
-        {
-            sc.signup();
-        }
-        else if (strcmp(buff, ">login")==0)
-        {
-            Status runningStatus = sc.login();
+        
+        if (strcmp(buff, ">signup") == 0) {
+            sn.signup();
+        } else if(strcmp(buff, ">login") == 0) {
+            Status runningStatus = sn.login();
 
-            if (runningStatus == Status::EXIT) {
+            if(runningStatus == Status::EXIT) {
                 return 0;
-            }
-            else if (runningStatus == Status::LOGOUT) {
+            } else if(runningStatus == Status::LOGOUT) {
                 continue;
             }
-        }
-        else if (strcmp(buff, ">exit") == 0)
-        {
+        }  else if(strcmp(buff, ">exit") == 0) {
             return 0;
         }
     }
 }
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
